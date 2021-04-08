a2268bf8c807f0c16f512e9155bb3da34595fcbe
Nina Z <zukowskanina1@gmail.com>
2021-04-08 02:14:53 +0200

    wszystko wsm działa (ewentualnie jest warning że funkcja implicitly defined, a tak to piękna sprawa), ewentualnie będą zmieniane wypisywane teksty B~)

--- header.h
+++ header.h
@@ -51,7 +51,21 @@ int isvalid(char* str, int len){
     if(i == (len)){return 1;}
     else{return 0;}
 }
+int is_a_num(char* str){
+    int i = 0;
+    int len = strlen(str);
 
+    while(('0'<=str[i] && str[i]<= '9') && i<len){i++;}
+    if(len == i){
+    return 1;}
+return 0;}
+
+int op_check(char* str){
+    if(str[0] == '+'){return 1;}
+    else if(str[0] == '-'){return 2;}
+    else if(str[0] == '*'){return 3;}
+    else{return 4;}
+}
 int extended_validity_checker(char* section1, char* key1){
     int sv = isvalid(section1, strlen(section1));
     int kv = isvalid(key1, strlen(key1));
@@ -68,7 +82,7 @@ return 1;}
 //******************ARGUMENT HANDLING********************
 //*******************************************************
 
-struct Name_returner* getting_section_key_name(int len_exp, char* expression, struct Name_returner* r){
+void getting_section_key_name(int len_exp, char* expression, struct Name_returner* r){
     int len_of_key;
     int len_of_section;
 
@@ -79,14 +93,14 @@ struct Name_returner* getting_section_key_name(int len_exp, char* expression, st
     char* key1 = malloc((len_of_key +1)*sizeof(char));
 
     strncpy(section1, expression,len_of_section);
-    strncpy(key1, expression+ len_of_section + 1,len_of_key);
+    strncpy(key1, expression + len_of_section + 1,len_of_key);
 
     key1[len_of_key] = '\0';
     section1[len_of_section] = '\0';
 
-    r->key = key1;
+    r->key = &key1[0];
     r->section =  section1;
-return r;}
+}
 //*******************************************************
 //***************** INSERTING FUNCTIONS *****************
 //*******************************************************
@@ -136,7 +150,7 @@ struct Returner* insert(int* state, char* line, struct Returner* r){
         else{
         int arr[3]; int size2; int size1;//end-start-end
         endstartend(arr, line);
-        size2 = arr[2] - arr[1] + 2;
+        size2 = arr[2] - arr[1];
         size1 = arr[0] + 1;
         char * key = malloc((size1+1)*sizeof(char));
         char * value = malloc((size2+1)*(sizeof(char)));
@@ -144,6 +158,7 @@ struct Returner* insert(int* state, char* line, struct Returner* r){
         value = strncpy(value,line + arr[1]+1,size2);
         key[size1] = '\0';
         value[size2] = '\0';
+        //printf("val= %s length of it:%d\n", value ,size2);
         r = add_entry(key, value, r);
         }
     }
@@ -198,7 +213,6 @@ return head;}
 char* klucznik(char* sname, char* ename, struct Section* head){
     struct Section* curs; //current section
     struct Entry* cure; //current entry
-    char* value;
     int val_len;
     curs = head;
     while(strcmp(curs->name,sname) != 0){
@@ -220,23 +234,57 @@ char* klucznik(char* sname, char* ename, struct Section* head){
             return NULL;
             }
     }
-    val_len = strlen(cure->value);
-    value = malloc((val_len+1)*sizeof(char));
-    strcpy(value, cure->value);
-    value[val_len] = '\0';
-return value;}
+return cure->value;
+}
 
 //*******************************************************
 //************** O P E R A T I O N S ********************
 //*******************************************************
 /// for the grade of 5
 
-char* operations(char* section1, char* key1, char* operation, char* section2, char*key2){
-    char* output = "NO TUTAJ JESZCZE BEDZIE OGARNIANE";
+void op_num(char* section1, char* key1, char* val1, char* operation, char* section2, char*key2, char* val2){
+    long int nval1 = atol(val1);
+    long int nval2 = atol(val2);
+    long int result;
 
-return output;}
+    switch(op_check(operation)){
+    case 1:
+        result = nval1 + nval2;
+        break;
+    case 2:
+        result = nval1 - nval2;
+        break;
+    case 3:
+        result = nval1 * nval2;
+        break;
+    case 4:
+        result = nval1 / nval2;
+        break;
+        }
+    printf("%s.%s %s %s.%s  = %ld\n", section1, key1, operation, section2, key2, result);
+}
 
+void op_str(char* section1, char* key1, char* val1, char* operation, char* section2, char*key2, char* val2){
 
+    if(op_check(operation) == 1){
+        int leng_w = strlen(val1) + strlen(val2);
+        char* result = malloc(leng_w +1);
+        strcpy(result, val1);
+        strcat(result, val2);
+        result[leng_w] = '\0';
+        printf("%s.%s + %s.%s  = %s\n", section1, key1, section2, key2, result);}
+    else{ printf("%s.%s %s %s.%s is an invalid operation\n", section1, key1,operation, section2, key2);}
+}
+
+void operations(struct Section* head,char* section1, char* key1, char* operation, char* section2, char*key2){
+
+    char* value1 = klucznik(section1,key1, head);
+    char* value2 = klucznik(section2,key2, head);
+
+    if(is_a_num(value1) && is_a_num(value2)){op_num(section1,key1,value1,operation, section2,key2, value2);}
+    else{op_str(section1,key1,value1,operation, section2,key2, value2);}
+ //free(value1); free(value2);
+ }
 
 //*******************************************************
 //************** TOTALNE ZNISCZENIE *********************
@@ -249,12 +297,9 @@ void uroboros(struct Section* head){
     struct Entry* tempe; // -||-
 
     curs = head;
-
     while(curs){
     cure = curs->first;
-        //printf("deleting the section %s\n", curs->name);
         while(cure){
-            //printf("deleting the entry: name: %s, value: %s\n", cure->key, cure->value);
             free(cure->value);
             free(cure->key);
             tempe = cure->next;
@@ -267,6 +312,6 @@ void uroboros(struct Section* head){
         curs = temps;
     }
 
-printf("MEMORY SUCCESFULLY FREED!\n"); // to tylko do sprawdzania od razu czy na pewno nic siê nie wywala
+//printf("MEMORY SUCCESFULLY FREED!\n"); // to tylko do sprawdzania od razu czy na pewno nic siê nie wywala
 }
 #endif // _HEADER_H_
--- main.c
+++ main.c
@@ -4,13 +4,14 @@
 #include "header.h"
 
 
-int main( int argc, char *argv[] )  {
+int main( int argc, char **argv)  {
 char* filename;
+filename = malloc((strlen(argv[1])+1)*(sizeof(char)));
 char* expression;
-char* section1;char* key1;
+char* section1;
+char* key1;
 char* val1;
-
-filename = argv[1];
+strcpy(filename,argv[1]);
 
 //FILE HANDLING
 FILE* fp;
@@ -27,21 +28,24 @@ expression[len_exp] = '\0';
 //tutaj to typowe szukañsko
 if(argc == 3){
 
-    struct Name_returner* r = malloc(sizeof(struct Name_returner*));
-    r = getting_section_key_name(len_exp,expression, r);
+    struct Name_returner* r;
+    r = malloc(sizeof(struct Name_returner));
+    r->key = NULL;
+    r->section = NULL;
+    getting_section_key_name(strlen(expression),expression, r);
     section1 = r->section;
     key1 = r->key;
 
     if( extended_validity_checker(section1, key1)== 1){
-    val1 = klucznik(section1, key1,head);
+    val1 = klucznik(section1, key1, head);
     if(val1){printf("The value: %s\n", val1);}
     }
     //freeing
     free(key1); free(section1); free(r);
 }
 else if(argc == 4){//tutaj to specjalne
-    struct Name_returner* r1 = malloc(sizeof(struct Name_returner*));
-    struct Name_returner* r2 = malloc(sizeof(struct Name_returner*));
+    struct Name_returner* r1 = malloc(sizeof(struct Name_returner));
+    struct Name_returner* r2 = malloc(sizeof(struct Name_returner));
     char* expression1; char* expression2;
     char* operation; char* output;
 
@@ -50,26 +54,24 @@ else if(argc == 4){//tutaj to specjalne
     operation = strtok(NULL, " ");
     expression2 = strtok(NULL, " ");
 
-    r1 = getting_section_key_name(strlen(expression1),expression1, r1);
+    getting_section_key_name(strlen(expression1),expression1, r1);
     section1 = r1->section;
     key1 = r1->key;
 
-    r2 = getting_section_key_name(strlen(expression2),expression2, r2);
+    getting_section_key_name(strlen(expression2),expression2, r2);
     section2 = r2->section;
     key2 = r2->key;
 
-    output = operations(section1, key1, operation, section2, key2);
-    printf("the outcome: %s\n", output);
+    operations(head, section1, key1, operation, section2, key2);
 
     //freeing the memory
     free(key1); free(key2); free(section1); free(section2); free(r1); free(r2);
 
 }
-
 fclose(fp);
 free(expression);
 uroboros(head);
-
+free(filename);
 return 0;}
 
 
